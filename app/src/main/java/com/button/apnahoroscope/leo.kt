94bc18ecc234b01c1de6f6e3c2de788a884f3bde
Sonal Singh <sonal1201kumar@gmail.com>
2023-03-13 17:08:48 +0530

    horoscope project files

---
+++ app/src/main/java/com/button/apnahoroscope/leo.kt
@@ -0,0 +1,11 @@
+package com.button.apnahoroscope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class leo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leo)
+    }
+}
--- app/src/main/java/com/button/apnahoroscope/secondpage.kt
+++ app/src/main/java/com/button/apnahoroscope/secondpage.kt
@@ -1,8 +1,11 @@
 package com.button.apnahoroscope
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.RelativeLayout
 
 class secondpage : AppCompatActivity() {
@@ -11,10 +14,22 @@ class secondpage : AppCompatActivity() {
         setContentView(R.layout.activity_secondpage)
 
         val ariess = findViewById<RelativeLayout>(R.id.aries)
+        val leoss = findViewById<RelativeLayout>(R.id.leos)
+        val callus = findViewById<Button>(R.id.con)
 
         ariess.setOnClickListener {
-            intent = Intent(applicationContext,aries::class.java)
+            intent = Intent(this,aries ::class.java )
             startActivity(intent)
         }
+        leoss.setOnClickListener {
+            intent = Intent(this,leo ::class.java)
+            startActivity(intent)
+        }
+        callus.setOnClickListener{
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data=Uri.parse("tel: 5387289")
+            startActivity(intent)
+        }
+
     }
 }
